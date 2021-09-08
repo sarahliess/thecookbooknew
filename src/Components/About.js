36b9daf623ad096d5e6f20c5e9ff32e305f213db
Sarah Ließ <sarah.liess@hotmail.de>
2021-09-08 14:20:02 +0200

    gh-pages

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,24 +3,24 @@ function About() {
     <div className="About">
       <h1>ABOUT US</h1>
       <p>
-        Sugar plum cupcake apple pie bonbon sweet roll soufflé. Cotton candy
-        cake oat cake jelly-o pie cheesecake bear claw shortbread fruitcake.
-        Pastry tootsie roll sweet lollipop pie. Tart cake apple pie lemon drops
-        soufflé biscuit wafer icing. Icing dessert cheesecake toffee gummies
-        fruitcake chupa chups macaroon. Tootsie roll cupcake pudding icing
-        shortbread soufflé. Cake shortbread danish caramels jelly dragée
-        cupcake. Cookie jelly-o marshmallow chupa chups wafer chupa chups
-        marshmallow oat cake caramels. Macaroon cake marshmallow macaroon muffin
-        sugar plum. Marshmallow gummi bears fruitcake powder chocolate cake.
-        Sweet roll macaroon toffee lemon drops carrot cake donut. Oat cake
-        bonbon candy canes pastry brownie cake sweet pastry marzipan. Tootsie
-        roll soufflé macaroon topping pudding cookie topping. Dragée macaroon
-        sweet roll dessert cake marzipan carrot cake. Caramels wafer tiramisu
-        bonbon dragée gummies. Carrot cake halvah tootsie roll powder tiramisu
-        biscuit icing powder. Chocolate cake cotton candy candy chocolate cake
-        biscuit. Gingerbread shortbread sweet jujubes marshmallow. Cheesecake
-        brownie soufflé caramels caramels sweet marzipan biscuit gingerbread.
-        Toffee chupa chups gingerbread bonbon cheesecake. Toffee donut chocolate
+        Sugar cupcake apple pie bonbon sweet roll soufflé. Cotton candy cake oat
+        cake jelly-o pie cheesecake bear claw shortbread fruitcake. Pastry
+        tootsie roll sweet lollipop pie. Tart cake apple pie lemon drops soufflé
+        biscuit wafer icing. Icing dessert cheesecake toffee gummies fruitcake
+        chupa chups macaroon. Tootsie roll cupcake pudding icing shortbread
+        soufflé. Cake shortbread danish caramels jelly dragée cupcake. Cookie
+        jelly-o marshmallow chupa chups wafer chupa chups marshmallow oat cake
+        caramels. Macaroon cake marshmallow macaroon muffin sugar plum.
+        Marshmallow gummi bears fruitcake powder chocolate cake. Sweet roll
+        macaroon toffee lemon drops carrot cake donut. Oat cake bonbon candy
+        canes pastry brownie cake sweet pastry marzipan. Tootsie roll soufflé
+        macaroon topping pudding cookie topping. Dragée macaroon sweet roll
+        dessert cake marzipan carrot cake. Caramels wafer tiramisu bonbon dragée
+        gummies. Carrot cake halvah tootsie roll powder tiramisu biscuit icing
+        powder. Chocolate cake cotton candy candy chocolate cake biscuit.
+        Gingerbread shortbread sweet jujubes marshmallow. Cheesecake brownie
+        soufflé caramels caramels sweet marzipan biscuit gingerbread. Toffee
+        chupa chups gingerbread bonbon cheesecake. Toffee donut chocolate
         gummies tootsie roll candy wafer carrot cake. Pudding dragée sesame
         snaps bear claw cookie carrot cake. Halvah chocolate bar marshmallow
         chocolate bar lemon drops marshmallow cake jujubes. Marshmallow sweet
